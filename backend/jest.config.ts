fb3a6be487bf5450c4b5f7eb5466dd6b31d129de
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-09-30 20:52:36 +0200

    add push husky

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -10,4 +10,12 @@ module.exports = {
     moduleNameMapper: {
         '^axios$': require.resolve('axios'),
     },
+    coverageThreshold: {
+        global: {
+            branches: 30,
+            functions: 30,
+            lines: 30,
+            statements: 30,
+        },
+    },
 }
