fcf5357251b7dc476e886680c5c81b46e38e9349
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-10-05 23:34:28 +0200

    add register user

---
+++ backend/tests/unit/user.controller.test.ts
@@ -0,0 +1,6 @@
+import ClientController from "../../controllers/clientController";
+
+ClientController.getClients = jest.fn();
+ClientController.createClient = jest.fn();
+ClientController.deleteClient = jest.fn();
+ClientController.updateClientById = jest.fn();
