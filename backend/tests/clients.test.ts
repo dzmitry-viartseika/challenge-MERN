5ebde847a20a8cb59232f54f2365b5126b64121b
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-10-07 19:36:58 +0200

    add register user

--- backend/tests/clients.test.ts
+++ backend/tests/clients.test.ts
@@ -1,7 +1,71 @@
+import request from 'supertest';
+import app from '../index'
+
+const STUB_CLIENT_RESPONSE = {
+    firstName: 'Test',
+    lastName: 'TEst',
+    email: 'test@gmail.com'
+}
+
 describe('deleteClient method', () => {
-    it('has a deleteClient method', () => {
-        expect(true).toBeTruthy()
+    it('has a deleteClient method', async () => {
+        const response = await request(app).get('/api/v1/clients');
+        expect(response.statusCode).toBe(200)
     })
+
+    it('has a deleteClient method', async () => {
+        const response = await request(app).post('/api/v1/clients').send(STUB_CLIENT_RESPONSE)
+        expect(response.statusCode).toBe(200);
+
+        const responseData = {
+            ...response.body,
+            client: {
+                ...response.body.client,
+                ...STUB_CLIENT_RESPONSE,
+            }
+        }
+        console.log('responseData', responseData)
+        expect(response.body).toMatchObject(responseData)
+    })
+
+    it('tst', async () => {
+        const ID = '1';
+        const response = await request(app).delete(`/api/v1/clients/${ID}`)
+        expect(response.statusCode).toBe(500)
+    })
+
+    it.only('tst', async () => {
+        const ID = '65132efa381b68d9612f9acd';
+        const response = await request(app).delete(`/api/v1/clients/${ID}`)
+        expect(response.statusCode).toBe(200)
+    })
+
+
+    // it('should catch missing required fields', async () => {
+    //     const response = await request(app).post('/api/v1/clients').send({
+    //         firstName: 'Test',
+    //         lastName: '',
+    //         email: 'test@gmail.com'
+    //     })
+    //     expect(response.statusCode).toBe(400)
+    //     expect(response.body).toStrictEqual({
+    //         code: 400,
+    //         message: 'Fill in required fields',
+    //     })
+    // })
+
+    // it('should catch missing required fields', async () => {
+    //     const response = await request(app).post('/api/v1/clients').send({
+    //         firstName: 'Test',
+    //         lastName: '',
+    //         email: 'test@gmail.com'
+    //     })
+    //     expect(response.statusCode).toBe(400)
+    //     expect(response.body).toStrictEqual({
+    //         code: 400,
+    //         message: 'Fill in required fields',
+    //     })
+    // })
 })
 
 import express from "express";
--- backend/tests/unit/user.controller.test.ts
+++
@@ -1,6 +0,0 @@
-import ClientController from "../../controllers/clientController";
-
-ClientController.getClients = jest.fn();
-ClientController.createClient = jest.fn();
-ClientController.deleteClient = jest.fn();
-ClientController.updateClientById = jest.fn();
