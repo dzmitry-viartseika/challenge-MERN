d1e8c02c5f205428e009f3bfcc886dd96e55c7b6
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-11-27 18:37:36 +0100

    feat: add idle implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,24 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastProvider } from './context/ToastContext';
 import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {routes} from "./constants/routes";
+import {useIdleTimer} from "react-idle-timer";
 
 const queryClient = new QueryClient();
 const App = () => {
+    const navigate = useNavigate();
+
+    const onIdle = () => {
+        navigate(routes.LOGIN)
+    }
+
+    useIdleTimer({
+        onIdle,
+        timeout: 900_000,
+        throttle: 500
+    })
     return (
       <QueryClientProvider client={queryClient}>
               <ToastProvider>
@@ -14,7 +29,6 @@ const App = () => {
                       <Outlet />
                   </main>
               </ToastProvider>
-
               <ReactQueryDevtools initialIsOpen={true} />
       </QueryClientProvider>
   );
