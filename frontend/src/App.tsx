15388213814a0cae99c3772e2afea0a3499dde82
Dzmitry Viartseika <verteyko1990@gmail.com>
2023-11-27 18:41:28 +0100

    feat: add idle implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastProvider } from './context/ToastContext';
 import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
-import React, {useState} from "react";
+import React from "react";
 import {useNavigate} from "react-router-dom";
 import {routes} from "./constants/routes";
 import {useIdleTimer} from "react-idle-timer";
